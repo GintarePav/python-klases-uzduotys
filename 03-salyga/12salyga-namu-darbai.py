2b6b70177ae98a8f08f327ad47340ec2f1dae788
GintarePav <gintare.pavilaviciute@gmail.com>
2024-02-20 22:31:31 +0200

    namu darbai: skaiciuotuvas

--- 03-salyga/12salyga-namu-darbai.py
+++ 03-salyga/12salyga-namu-darbai.py
@@ -6,3 +6,31 @@
 # 4. Ivedamas skaicius b
 # 5. Gaunamas rezultatas
 # Naudoti tiesini algoritma, salygas ir funkcijas
+
+def naudotiSkaiciuotuva() :
+    skaicius = int(input("Iveskite skaiciu: "))
+    operacija = input("Iveskite operacija: ")
+
+    def ivestiAntra():
+        skaiciusDu = int(input("iveskite antra skaiciu: "))
+        return skaiciusDu
+
+    if operacija == "+" :
+        return skaicius + ivestiAntra()
+    elif operacija == "-" :
+        return skaicius - ivestiAntra()
+    elif operacija == "/" :
+        return skaicius - ivestiAntra()
+    elif operacija == "*" :
+        return skaicius * ivestiAntra()
+    elif operacija == "^" :
+        return skaicius ** ivestiAntra()
+    elif operacija == "q" :
+        return skaicius ** 0.5
+    else :
+        print("Operacija negalima, bandykite dar karta.")
+        return naudotiSkaiciuotuva()
+
+skaiciuotuvoAtsakymas = naudotiSkaiciuotuva()
+
+print(f'Atlikus jusu suvestus veiksmus skaiciuotuvas gavo atsakyma: {skaiciuotuvoAtsakymas}')
